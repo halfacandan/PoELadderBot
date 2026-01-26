6f75aaa4255996633bf8e6f2119e6c22a05e3625
halfacandan <amanofdistinction@gmail.com>
2026-01-26 15:58:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,139 @@
+# PoE Ladder Bot
+
+This project creates a Discord bot which interacts with PoE Ladder's bot api.
+
+## Commands
+
+| Command  | Description                                                |
+| -------- | ---------------------------------------------------------- |
+| /about   | Info on how to add new functionality to the PoE Ladder Bot |
+| /help    | List all available commands                                |
+| /ladders | List the monitored PoE Ladder leagues                      |
+
+## Local Development of the Bot
+
+The bot runs in 
+
+### Before You Begin
+
+This guide uses Visual Studio Code but there are many alternatives. Install these pre-requisites before you begin:
+
+1. [Download & Install Git](https://git-scm.com/downloads)
+2. [Configure Git](https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup)
+3. [Download & Install Visual Studio Code](https://code.visualstudio.com/download)
+4. [Download & Install Node](https://nodejs.org/en) - This includes npm (node package manager) which is used in the subsequent steps below
+5. Open Visual Studio Code
+6. Clone this Repository:
+    1. The **Welcome** screen should include an option to **Clone Git Repository...**. Click this option
+    2. Paste in this link then hit the **Enter** key: https://github.com/halfacandan/PoELadderBot.git
+7. Open a terminal session in Visual Studio Code by opening the **Terminal** menu and selecting the **New Terminal** option
+8. Install all required packages by running the following command in the terminal (it may take some time to download them all):
+    
+    ```bash
+    npm install
+    ```
+
+### Setting up the Bot in Discord
+
+Follow the steps outlined in this [Discord Bot Tutorial: Get your bot up and running in under 5 minutes](https://codeburst.io/discord-bot-tutorial-2020-a8a2e37e347c).
+
+1. Go to the [Discord Developer Portal](https://discord.com/developers/applications)
+2. Click the **New Application** button in the top-right corner of the screen
+
+    ![Click the "New Application" button](resources/01-click-new-application-button.png)
+
+3. Give your bot a name, accept the T&Cs then click the **Create** button (as shown below)
+
+    ![Name your Application](resources/02-name-application.png)
+
+4. Add a logo and a description for the application
+
+    ![Add basic information](resources/03-add-basic-application-info.png)
+
+5. Click the **Save Changes** button
+6. Open the **OAuth2** menu
+
+    ![Open OAuth2 Menu](resources/04-open-oauth2-menu.png)
+
+7. Copy the **Client ID** value and paste it into the **CLIENT_ID** value in the `.env.local` file in this repository
+8. Click the **Reset Secret** button beneath the **Client Secret** heading to display a secret value
+9. Copy the **Client Secret** value and paste it into the **PRIV_KEY** value in the `.env.local` file in this repository
+10. Open the **Bot** menu
+
+    ![Open Bot Menu](resources/05-open-bot-menu.png)
+
+11. Click the **Reset Token** button
+12. Copy the token value and paste it into the **BOT_TOKEN** value in the `.env.local` file in this repository
+13. Scroll down the **Bot** page to the **Privileged Gateway Intents** section and enable the following options:
+
+    - Message Content Intent
+    - Server Members Intent
+
+14. Click the **Save Changes** button
+15. Open the **Installation** menu
+
+    ![Open Installation Menu](resources/06-open-installation-menu.png)
+
+16. Uncheck the **User Install** option under the **Installation Contexts** menu (leave **Guild Install** checked)
+17. Scroll down to the **Install Link** and copy it.
+18. Add "&permissions=2147593280&scope=bot%20applications.commands" to the end of the url e.g. 
+    ```text
+    https://discord.com/oauth2/authorize?client_id=123455667890&permissions=2147593280&scope=bot%20applications.commands
+    ```
+19. Open the revised url in a browser
+20. Select the **Add to Server** option
+21. Pick which server you'd like to add the bot to then click the **Authorize** button (as shown below):
+
+    ![Install bot](resources/07-select-server-and-authorise.png)
+
+**NOTE:** If you wish to replicate the permissions, you can use the **Bot Permissions** section on the **Bot** page and use the following:
+
+- General Permissions:
+  - View Channels
+- Text Permissions: 
+  - Send Messages
+  - Manage Messages
+  - Attach Files
+  - Read Message History
+  - Add reactions
+  - Use Slash Commands
+
+### Finding your Discord Server's Id
+
+We need one last piece of configuration set up before we can run the bot:
+
+1. Open the Discord desktop client
+2. Click the cog icon next to your profile (in the bottom-left corner of the screen)
+
+    ![Access User Settings](resources/08-access-user-settings.png)
+
+3. Scroll down the left-hand menu and select **Advanced** (as shown below)
+4. Enable **Developer Mode**
+
+    ![Enable Developer Mode](resources/09-enable-developer-mode.png)
+
+5. You can now right-click on any of the servers which you have joined and use the **Copy Server ID** option
+
+    ![Copy Server Id](resources/10-copy-server-id.png)
+
+6. Copy the server id value and paste it into the **SERVER_ID** value in the `.env.local` file in this repository
+
+### Running the Bot Locally
+
+Once you have set up an application in Discord, you can host the bot on your computer by doing the following:
+
+1. Open the Discord desktop client
+2. Open the server which you have installed the bot on
+3. Open Visual Studio Code
+4. Open a terminal session in Visual Studio Code by opening the **Terminal** menu and selecting the **New Terminal** option
+5. Run this command:
+
+    ```bash
+    npm run dev
+    ```
+
+6. You should see the message **PoE Ladder Bot is online** in the terminal
+7. The bot will now show as online in your Discord Server
+8. Test it with the slash command **/about**
+
+    ![Bot responds to a slash command](resources/11-bot-responds-to-command.png)
---
+++ modules/messages.js
@@ -0,0 +1,296 @@
+const LineBreak = "\n\uFEFF";
+
+module.exports = {
+    BotError: function(){
+        return "**\\*Blip\\*** *\\*Blip\\** ***\\*Blip\\**** End of Cheese Error";
+    },
+    AboutThisBot: async function(){
+
+        const BotAuthorDiscordId = '342266718334353408';
+
+        let embeddedMessage = {
+            "embed": {
+                "title": ":robot:  PoE Ladder Bot",
+                "description": `The PoE Ladder Bot was created by <@!${BotAuthorDiscordId}>. You can submit pull requests to the public git repo if you'd like to contribute.${LineBreak}`,
+                "fields": [
+                    {
+                        "name": ":computer:  Git Repo",
+                        "value": " * [View the PoE Ladder Bot's node.js code](https://github.com/halfacandan/PoELadderBot)"
+                    }
+                ]
+            }
+        };
+
+        return embeddedMessage;
+    },
+    ListBotCommands: async function(botAboutCommand = "about"){
+
+        let commands =   [
+            `**/${botAboutCommand}** - Info on how to add new functionality to the PoE Ladder Bot`,
+            `**/ladder** - List the monitored PoE Ladder leagues`
+        ];
+
+        return commands;
+    },
+    PrintLadderGroup(title, ladders) {
+
+        if(ladders?.length == 0) return null;
+
+        return {
+            "name": title,
+            "value": 
+                ladders
+                    .sort((a, b) => (a.OrderBy ?? 0) - (b.OrderBy ?? 0))
+                    .map((ladder) =>  ` * ${ladder.name}`)
+                    .join("\n")
+        }
+    },
+    ListLadders: async function(ladders) {
+
+        if(ladders?.length == 0) return null; 
+
+        let fields = [
+            this.PrintLadderGroup(
+                "Path of Exile 1 (Events)",
+                ladders.filter(l =>
+                    !l.isPoe2 && l.registerable && l.event
+                )
+            ),
+            this.PrintLadderGroup(
+                "Path of Exile 1",
+                ladders.filter(l =>
+                    !l.isPoe2 && l.registerable && !l.hardcore && !l.event && !l.private && !l.ruthless && !l.trade
+                )
+            ),
+            this.PrintLadderGroup(
+                "Path of Exile 1 (Hardcore)",
+                ladders.filter(l =>
+                    !l.isPoe2 && l.registerable && l.hardcore && !l.event && !l.private && !l.ruthless && !l.trade
+                )
+            ),
+            this.PrintLadderGroup(
+                "Path of Exile 1 (Trade)",
+                ladders.filter(l =>
+                    !l.isPoe2 && l.registerable && !l.hardcore && !l.event && !l.private && !l.ruthless && l.trade
+                )
+            ),
+            this.PrintLadderGroup(
+                "Path of Exile 1 (Ruthless)",
+                ladders.filter(l =>
+                    !l.isPoe2 && l.registerable && l.ruthless
+                )
+            ),
+            this.PrintLadderGroup(
+                "Path of Exile 1 (Private Leagues)",
+                ladders.filter(l => 
+                    !l.isPoe2 && l.registerable && l.private
+                )
+            ),
+            this.PrintLadderGroup(
+                "Path of Exile 2",
+                ladders.filter(l =>
+                    l.isPoe2 && l.registerable
+                )
+            )
+        ];
+
+        let embeddedMessage = {
+            "embed": {
+                "title": ":ladder:  Active PoE Ladders",
+                "description": `PoE Ladder currently tracks these leagues:`,
+                "fields": fields.filter(l => l != null)
+            }
+        };
+
+        return embeddedMessage;
+    },
+    FixBulletPoints: function(text){
+
+        const bulletOne = "\uFEFF\u2001\u2022 ";
+        const bulletTwo = "\uFEFF\u2001\u2001\u2043 ";
+
+        // Unordered lists
+        text = text.replace(/ \* /g, bulletOne).replace(/ \*\* /g, bulletTwo);
+        // Ordered lists
+        text = text.replace(/ ([0-9]+(?:\.|\)) )/g, "\uFEFF\u2001$1").replace(/ ([0-9]+\.[0-9]+(?:\.|\)) )/g, "\uFEFF\u2001\u2001$1");
+
+        // Fix any incorrectly-escaped \uFEFF stringification issues
+        text = text.replace("\\uFEFF", "\uFEFF");
+
+        return text;
+    },
+    ParseEmbeddedMessage: async function(discord, embeddedMessage){
+
+        var attachment = null;
+
+        if(typeof embeddedMessage.embed.type === "undefined" || embeddedMessage.embed.type == null){
+            embeddedMessage.embed.type = "rich";
+        }
+
+        if(typeof embeddedMessage.embed.description !== "undefined" && embeddedMessage.embed.description != null){
+            embeddedMessage.embed.description = this.FixBulletPoints(embeddedMessage.embed.description);
+        }
+
+        if(typeof embeddedMessage.embed.fields !== "undefined" && embeddedMessage.embed.fields != null){
+            for(var i = 0; i < embeddedMessage.embed.fields.length; i++){
+                if(typeof embeddedMessage.embed.fields[i].value !== "undefined") {
+                    let fixedFieldValue = this.FixBulletPoints(embeddedMessage.embed.fields[i].value);
+                    embeddedMessage.embed.fields[i].value = fixedFieldValue;
+                }
+            }
+        }
+
+        if((typeof embeddedMessage.embed.image === "undefined" || embeddedMessage.embed.image == null) 
+                && typeof embeddedMessage.embed.table !== "undefined" && embeddedMessage.embed.table != null){
+
+            // The "text-to-image" npm package causes random crashes on Raspberry Pi 4 so use "text2png"
+            const text2png = require('@thatiemsz/text2png');            
+            let imageStream = text2png(embeddedMessage.embed.table, {
+                font: '16px Courier',
+                color: 'white',
+                bgColor: '#2f3136', // Discord Dark Gray
+                lineSpacing: 0,
+                padding: 0,
+                output: 'buffer'
+            });
+
+            if(typeof embeddedMessage.embed.title !== "undefined" && embeddedMessage.embed.title != null){
+                imageName = embeddedMessage.embed.title.trim().toLowerCase().replace(/\s/g, "_").replace(/[^a-zA-Z0-9]/ig, "");
+            } else {
+                imageName = Math.random().toString(36).replace(/[^a-z]+/ig, '').substr(0,5);
+            }
+
+            attachment = new discord.AttachmentBuilder(imageStream, { name: `${imageName}.png`});
+            if(attachment != null){
+                embeddedMessage.embed.image = {
+                    "url": `attachment://${imageName}.png`
+                }
+            }
+        }
+
+        if(attachment == null){
+            return embeddedMessage;
+        } else {
+            let message = { 
+                "embeds": [embeddedMessage.embed], 
+                "files": [attachment]
+            };
+            return message;
+        }
+    },
+    ReactToMessageAsync: async function (bot, message, reactions){
+
+        if(bot == null || message == null || reactions == null) return;
+
+        if(typeof(reactions) === "string"){
+            reactions = Array(reactions);
+        }
+
+        var msg = message;
+        if(message.constructor.name != "InteractionResponse"){
+            msg = await message.channel.messages.fetch(message.id);
+        }
+
+        for(var i=0; i < reactions.length; i++){
+            let emojiCode = await this.GetEmojiCodeAsync(bot, reactions[i]);
+            if(emojiCode != null){
+                await msg.react(emojiCode);
+            }
+        }
+    },    
+    GetEmojiCodeAsync: async function (bot, emojiShortcode){
+        // https://discordjs.guide/popular-topics/reactions.html#custom-emojis
+
+        if(emojiShortcode.match(/:[^:]+:$/g) != null && bot != null){
+            var emoji = await bot.emojis.cache.find(emoji => emoji.name == emojiShortcode.replace(/:|:$/g,''));
+            if(typeof(emoji) !== "undefined") {
+                // This is a custom emoji
+                return emoji.id;
+            } else {
+                // This is an invalid custom emoji
+                return null;
+            }
+        } else {
+            // This is a unicode emoji
+            return emojiShortcode;
+        }
+    },
+    SendReplies: async function(discord, bot, userMessage, replies, reactions = null, replyToPerson = false, reactToMessageNumber = null, isInteraction = false){
+
+        if(replies != null){
+
+            var message;
+            var finalReplyMessage;
+            var reacted = false;
+            var messages = [];
+
+            for(var i=0; i < replies.length; i++){
+                if(!isInteraction && (replyToPerson || userMessage == null || typeof userMessage.channel === "undefined" || userMessage.channel == null)){
+                    if(typeof replies[i] === "string") {
+                        message = "\n" + replies[i];
+                        messages.push(await userMessage.reply(replies[i], { split: true }));
+                    } else {
+                        message = await this.ParseEmbeddedMessage(discord, replies[i]);
+                        messages.push(await userMessage.reply({ embeds: [message.embed] }));
+                    }                    
+                } else {
+                    if(typeof replies[i] === "string") {
+                        if(isInteraction){
+                            if(i == 0){
+                                await userMessage.reply(replies[i], { split: true });
+                            } else {
+                                await userMessage.followUp(replies[i], { split: true });
+                            }                            
+                            messages.push(await userMessage.fetchReply());
+                        } else {
+                            messages.push(await userMessage.channel.send(replies[i], { split: true }));
+                        }
+                    } else {
+                        var messageFormatted;
+                        if(typeof replies[i].embed === "undefined"){
+                            messageFormatted = { 
+                                "content": replies[i].content, 
+                                "embeds": [{
+                                    "image": {
+                                        "url": replies[i].file
+                                    }
+                                }]
+                            };
+                        } else {
+                            message = await this.ParseEmbeddedMessage(discord, replies[i]);
+                            if(message.files != null){
+                                messageFormatted = message;
+                            } else {
+                                messageFormatted = { 
+                                    embeds: [message.embed] 
+                                };
+                            }  
+                        }
+                        if(isInteraction){
+                            if(i == 0){
+                                await userMessage.reply(messageFormatted);
+                            } else {
+                                await userMessage.followUp(messageFormatted);
+                            }
+                            messages.push(await userMessage.fetchReply());
+                        } else {
+                            messages.push(await userMessage.channel.send(messageFormatted));
+                        }
+                    }                
+                }
+                
+                finalReplyMessage = messages[i];
+
+                if(reactToMessageNumber == i){                    
+                    let replyMessage = Array.isArray(finalReplyMessage) ? finalReplyMessage[0] : finalReplyMessage;
+                    await this.ReactToMessageAsync(bot, replyMessage, reactions);
+                    reacted = true;
+                }
+            }
+            if(reactions != null && reacted != true){
+                let replyMessage = Array.isArray(finalReplyMessage) ? finalReplyMessage[0] : finalReplyMessage;
+                await this.ReactToMessageAsync(bot, replyMessage, reactions);
+            }
+        }
+    }
+}
